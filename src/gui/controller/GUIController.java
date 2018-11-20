82c330a22b8d019e89abac45747277c2ae8439d2
_gavbob <gsct2002@gmail.com>
2018-11-20 12:49:34 -0700

    Connected panel frame and controller

--- src/gui/controller/GUIController.java
+++ src/gui/controller/GUIController.java
@@ -1,15 +1,18 @@
 package gui.controller;
 
 import gui.model.Minion;
+import gui.view.FirstFrame;
 
 public class GUIController
 {
 	private Minion myMinion;
+	private FirstFrame appFrame;
 	
 	public GUIController()
 	{
 		myMinion = new Minion();
-		
+
+		appFrame = new FirstFrame(this);
 	}
 	
 	public void start()
--- src/gui/view/FirstFrame.java
+++ src/gui/view/FirstFrame.java
@@ -1,11 +1,28 @@
 package gui.view;
 
 import javax.swing.JFrame;
+import gui.controller.GUIController;
 
 public class FirstFrame extends JFrame
 {
-	public FirstFrame()
+	private GUIController appController;
+	private FirstPanel appPanel;
+	
+	public FirstFrame(GUIController appController)
 	{
+		super();
+		this.appController = appController;
+		this.appPanel = new FirstPanel(appController);
 		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(800, 800);
+		this.setTitle("GU(cc)I Gang");
+		this.setResizable(true);
+		this.setVisible(true);
 	}
 }
