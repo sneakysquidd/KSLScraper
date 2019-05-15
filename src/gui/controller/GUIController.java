3f9c578fc2138979f033025287bb439d501dd450
_gavbob <gsct2002@gmail.com>
2019-05-15 13:38:20 -0600

    worked on search method

--- src/gui/controller/GUIController.java
+++ src/gui/controller/GUIController.java
@@ -1,8 +1,13 @@
 package gui.controller;
 
+import java.math.BigDecimal;
 import java.net.URLEncoder;
+import java.util.List;
 
+import com.fa;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import gui.model.Minion;
@@ -31,9 +36,26 @@ public class GUIController
 		client.getOptions().setCssEnabled(false);  
 		client.getOptions().setJavaScriptEnabled(false);  
 		try {  
-		  String searchUrl = "https://classifieds.ksl.com/search?category[]=&subCategory[]=&keyword=" + URLEncoder.encode(Item, "UTF-8") + "&priceFrom=$1&priceTo=$2&zip=" + URLEncoder.encode(zipCode, "UTF-8") + "&miles=25&sellerType[]=&marketType[]=Sale&hasPhotos[]=&postedTime[]=";
+		  String searchUrl = "https://classifieds.ksl.com/search?category[]=&subCategory[]=&keyword=%24" + URLEncoder.encode(Item, "UTF-8") + "&priceFrom=%24" + URLEncoder.encode(fromPrice, "UTF-8") + "&priceTo=" + URLEncoder.encode(toPrice, "UTF-8") + "&zip=" + URLEncoder.encode(zipCode, "UTF-8") + "&miles=25&sellerType[]=&marketType[]=Sale&hasPhotos[]=&postedTime[]=";
 		  HtmlPage page = client.getPage(searchUrl);
-		}catch(Exception e){
+		  
+		  List<HtmlElement> items = (List<HtmlElement>) page.getByXPath("//span[@class='rows']/p") ;
+			if(items.isEmpty()){
+				System.out.println("No items found !");
+			}else{
+				for(HtmlElement item : items){
+					HtmlAnchor itemAnchor = ((HtmlAnchor) item.getFirstByXPath(".//span[@class='txt']/span[@class='pl']/a"));
+
+					String itemName = itemAnchor.asText();
+					String itemUrl = itemAnchor.getHrefAttribute() ;
+
+					HtmlElement spanPrice = ((HtmlElement) item.getFirstByXPath(".//span[@class='txt']/span[@class='l2']/span[@class='price']")) ;
+					// It is possible that an item doesn't have any price
+					String itemPrice = spanPrice == null ? "no price" : spanPrice.asText() ;
+					System.out.println( String.format("Name : %s Url : %s Price : %s", itemName, itemPrice, itemUrl));
+				}
+			}
+		}catch(Exception e){i
 		  e.printStackTrace();
 		}
 		}
