819f31212ddfaacf7099a9f954edf3d65917cd08
sneakysquidd <gsct2002@gmail.com>
2019-05-19 14:27:22 -0600

    Added test to find certain divs on KSL site

--- src/gui/controller/GUIController.java
+++ src/gui/controller/GUIController.java
@@ -4,30 +4,25 @@ import java.math.BigDecimal;
 import java.net.URLEncoder;
 import java.util.List;
 
-import com.fa;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-import gui.model.Minion;
 import gui.view.WebFrame;
 
 public class GUIController
 {
-	private Minion myMinion;
 	private WebFrame appFrame;
 	
 	public GUIController()
 	{
-		myMinion = new Minion();
-
 		appFrame = new WebFrame(this);
 	}
 	
 	public void start()
 	{
-		
+		searchForItem("iphone", "1", "1", "84020");
 	}
 	
 	public static void searchForItem(String Item, String fromPrice, String toPrice, String zipCode)
@@ -36,26 +31,25 @@ public class GUIController
 		client.getOptions().setCssEnabled(false);  
 		client.getOptions().setJavaScriptEnabled(false);  
 		try {  
-		  String searchUrl = "https://classifieds.ksl.com/search?category[]=&subCategory[]=&keyword=%24" + URLEncoder.encode(Item, "UTF-8") + "&priceFrom=%24" + URLEncoder.encode(fromPrice, "UTF-8") + "&priceTo=" + URLEncoder.encode(toPrice, "UTF-8") + "&zip=" + URLEncoder.encode(zipCode, "UTF-8") + "&miles=25&sellerType[]=&marketType[]=Sale&hasPhotos[]=&postedTime[]=";
+		 String searchUrl = "https://classifieds.ksl.com/search?category[]=&subCategory[]=&keyword=%24" + Item + "&priceFrom=%24" + fromPrice + "&priceTo=" + toPrice + "&zip=" + zipCode + "&miles=25&sellerType[]=&marketType[]=Sale&hasPhotos[]=&postedTime[]=";
 		  HtmlPage page = client.getPage(searchUrl);
-		  
-		  List<HtmlElement> items = (List<HtmlElement>) page.getByXPath("//span[@class='rows']/p") ;
-			if(items.isEmpty()){
+		  System.out.println(searchUrl);
+
+            final List<?> divs = page.getByXPath("//div");
+            for(int i=0;i<divs.size();i++)
+            {
+                System.out.println(divs.get(i));
+            }
+
+            List<HtmlElement> items =  page.getByXPath("//section[@class='listing-item normal']");
+			if(divs.isEmpty()){
 				System.out.println("No items found !");
 			}else{
 				for(HtmlElement item : items){
-					HtmlAnchor itemAnchor = ((HtmlAnchor) item.getFirstByXPath(".//span[@class='txt']/span[@class='pl']/a"));
 
-					String itemName = itemAnchor.asText();
-					String itemUrl = itemAnchor.getHrefAttribute() ;
-
-					HtmlElement spanPrice = ((HtmlElement) item.getFirstByXPath(".//span[@class='txt']/span[@class='l2']/span[@class='price']")) ;
-					// It is possible that an item doesn't have any price
-					String itemPrice = spanPrice == null ? "no price" : spanPrice.asText() ;
-					System.out.println( String.format("Name : %s Url : %s Price : %s", itemName, itemPrice, itemUrl));
 				}
 			}
-		}catch(Exception e){i
+		}catch(Exception e){
 		  e.printStackTrace();
 		}
 		}
