578acc645a0a89e95e9c861b16854cb9dc58f456
sneakysquidd <gsct2002@gmail.com>
2019-05-19 21:58:09 -0600

    Initialized and added all components of searchPanel

--- src/gui/view/WebPanel.java
+++ src/gui/view/WebPanel.java
@@ -11,6 +11,7 @@ public class WebPanel extends JPanel
 {
 	private CardLayout appLayout;
 	private GUIController appController;
+    private JPanel panels;
 
     private JTextField desiredFromPrice;
     private JTextField desiredToPrice;
@@ -22,17 +23,36 @@ public class WebPanel extends JPanel
     private JLabel itemLabel;
     private JLabel zipLabel;
 
+    private JButton searchButton;
 
+	private JPanel searchPanel;
+	private JPanel viewPanel;
 
-	
 	public WebPanel(GUIController appController)
 	{
 		super();
-        this.appController = appController;        
-        
+        this.appController = appController;
         appLayout = new CardLayout();
-       
+        panels = new JPanel(new CardLayout());
 
+        this.desiredFromPrice = new JTextField();
+        this.desiredToPrice = new JTextField();
+        this.desiredItem = new JTextField();
+        this.desiredZip = new JTextField();
+
+        this.fromLabel = new JLabel();
+        this.toLabel = new JLabel();
+        this.itemLabel = new JLabel();
+        this.zipLabel = new JLabel();
+
+        this.searchButton = new JButton();
+
+        this.searchPanel = new JPanel();
+        this.viewPanel = new JPanel();
+
+        //Adds the two panels to the cardlayout
+        panels.add(searchPanel, "KSL Search");
+        panels.add(viewPanel, "Results");
 
         setupPanel();
         setupLayout();
@@ -44,6 +64,16 @@ public class WebPanel extends JPanel
 		this.setPreferredSize(new Dimension(800, 600));
 		this.setLayout(appLayout);
 
+		//adds all components to the searchPanel
+		searchPanel.add(desiredFromPrice);
+		searchPanel.add(desiredToPrice);
+		searchPanel.add(desiredItem);
+		searchPanel.add(desiredZip);
+		searchPanel.add(fromLabel);
+		searchPanel.add(toLabel);
+		searchPanel.add(itemLabel);
+		searchPanel.add(zipLabel);
+		searchPanel.add(searchButton);
 	}
 	
 	private void setupLayout()
